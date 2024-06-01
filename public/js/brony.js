1514821bc114bbe3ca457f6b1e96cce8f0bbb6bb
unknown <hitechmag2013@mail.ru>
2024-06-01 18:22:23 +0500

    row&button

--- public/js/brony.js
+++ public/js/brony.js
@@ -15,21 +15,34 @@ var cinemaHall1 = {
 
       // запомнили номер текущего места
       var seatNumber = j + 1;
-
-      cinemaHallRow += '<div class="seat" data-row="' + // собираем места в ряду с учетом расстояния между сидениями.
+      //Если первое место то перед ним добавляем номер
+      if (seatNumber === 1){
+        cinemaHallRow +='<div class = "rowBefore">Ряд: '+ rowNumber+'</div>'+ '<div class="seat " data-row="' + // собираем места в ряду с учетом расстояния между сидениями.
         rowNumber + '" data-seat="' + // Записываем ноомер строки и номер сидения
         seatNumber + '">' + seatNumber + '</div>';
+      }
+
+      else{
+        cinemaHallRow += '<div class="seat" data-row="' + // собираем места в ряду с учетом расстояния между сидениями.
+        rowNumber + '" data-seat="' + // Записываем ноомер строки и номер сидения
+        seatNumber + '">' + seatNumber + '</div>';
+      }
     }
+   
+
     cinemaHallMap += cinemaHallRow + '<div class="passageBetween">&nbsp;</div>'; // собираем ряды
   }
   
   //заполняем в html зал номер 1
   $('.zal1').html(cinemaHallMap);
+ 
+  
   // тут по клику определяем что место выкуплено
   $('.seat').on('click', function(e) {
+    result = '';
     // если первый раз кликнули билет выкупили, 
     // если повторно значит вернули билет
-    $(e.currentTarget).toggleClass('bay');
+    $(e.currentTarget).toggleClass('buy');
     //показываем сколько билетов выкуплено
     showBaySeat();
   });
@@ -37,11 +50,22 @@ var cinemaHall1 = {
   function showBaySeat() {
     result = '';
     //ищем все места купленные и показываем список выкупленных мест
-    $.each($('.seat.bay'), function( item) {
+    const elements = document.querySelectorAll('.buy');
+    for ( let el of elements){
       result += '<div class="ticket">Ряд: ' +
-        $(item).data().row + ' Место:' +
-        $(item).data().seat + '</div>';
-    });
-  
+      el.getAttribute('data-row') + ' Место:' +
+      el.getAttribute('data-seat') + '</div>';
+    }
+    if (result==''){
+      let buttonBuy = document.querySelector('#buy');
+      buttonBuy.style.visibility = 'hidden';
+    }
+    else{
+      let buttonBuy = document.querySelector('#buy');
+      buttonBuy.style.visibility = 'visible';
+    }
+    
     $('.result').html(result);
   }
+
+
